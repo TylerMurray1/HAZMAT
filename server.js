3adfb03dcea133dc1a9f1575a079e74e4eb5d973
Sam Pennington <sam.pennington5@gmail.com>
2018-10-31 12:08:47 -0400

    Minor cleanup
    Got rid of gVars variable and replaced it with a variable read from the users.json file. Login is now dependent on this new variable. jsrender now broken. Suggest replacing jsrender with tables loaded from a new "materials.json" file. See TODO on sensor_calculations.html

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ var bodyParser = require("body-parser");
 var jsrender = require('jsrender');
 const CONFIG = require('./public/config.json');
 const app = express();
-const gVars = require('./public/scripts/globalVars');
+var userObj = require('./public/users.json');
 var fs = require('fs')
 
 //TODO: I'd like to make this a dictionary that associates IDs with user names - Sam
@@ -27,14 +27,16 @@ app.get('/sensor_calculation', function (req, res) {
     res.redirect('http://localhost:3000/');
   }
   else{
-    var tmpl = jsrender.templates('./sensor_calculation.html');
-    var html = tmpl.render({weight: "11", rfid_tag: "1234567"});
-    res.send(html);
+    res.sendFile(CONFIG.userFilePath+'/HAZMAT/public/sensor_calculation.html')
+    //var tmpl = jsrender.templates('./sensor_calculation.html');
+    //var html = tmpl.render({weight: "11", rfid_tag: "1234567"});
+    //res.send(html);
   }
 })
 
 app.post('/', function (req, res) {
-  if (gVars.idArray.includes(req.body.id_value)) {
+  console.log(Object.values(userObj).indexOf(req.body.id_value))
+  if(Object.values(userObj).indexOf(req.body.id_value) > -1){
     authenticated = true;
     res.redirect('http://localhost:3000/sensor_calculation');
   }
@@ -46,14 +48,10 @@ app.post('/', function (req, res) {
 })
 
 app.post('/saveJSON', (req, res) => {
-
-  console.log(typeof req.body);
-  // TODO: save req.body as users.json
   res.json({ ok: true });
   fs.writeFile('./public/users.json', JSON.stringify(req.body), function(err, data){
     if (err) console.log(err);
-    console.log("Successfully Written to File.");
-});
+  });
 });
 
 app.listen(CONFIG.port, () => console.log(`Example app listening on port ${CONFIG.port}!`));
