b0569a97d3d5c30f65fc9d5e9aef64c72c7b6002
Sam Pennington <sam.pennington5@gmail.com>
2018-11-12 15:17:25 -0500

    Added in Delete User Functionality

--- server.js
+++ server.js
@@ -30,7 +30,6 @@ app.get('/transaction', (req, res) => res.sendFile(CONFIG.userFilePath+'/HAZMAT/
 app.get('/removeItem', (req, res) => res.sendFile(CONFIG.userFilePath+'/HAZMAT/public/removeItem.html'))
 app.get('/returnItem', (req, res) => res.sendFile(CONFIG.userFilePath+'/HAZMAT/public/returnItem.html'))
 app.get('/admin', (req, res) => res.sendFile(CONFIG.userFilePath+'/HAZMAT/public/admin.html'))
-app.get('/newUser', (req, res) => res.sendFile(CONFIG.userFilePath+'/HAZMAT/public/newUser.html'))
 
 app.get('/weigh', function (req, res) {
 
@@ -99,13 +98,26 @@ app.post('/saveJSON', (req, res) => {
         console.log(err);
     } else {
     obj = JSON.parse(data);
-    console.log(typeof(req.body))
-    console.log(Object.keys(req.body));
     obj[Object.keys(req.body)[0]] = Object.values(req.body)[0]
+    var json = JSON.stringify(obj);
+    fs.writeFile('./public/users.json', json);
+  }});
+});
+
+app.post('/deleteUserData', (req, res) => {
+  res.json({ ok: true });
+  //
+  fs.readFile('./public/users.json', function readFileCallback(err, data){
+    if (err){
+        console.log(err);
+    } else {
+    obj = JSON.parse(data);
+    console.log(obj)
+    delete obj[Object.keys(req.body)[0]]
     console.log(obj)
     var json = JSON.stringify(obj);
     fs.writeFile('./public/users.json', json);
-    }});
-  });
+  }});
+});
 
 app.listen(CONFIG.port, () => console.log(`Example app listening on port ${CONFIG.port}!`));
