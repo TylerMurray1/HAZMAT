ea7ae0b9c386595d006aa1c5d4a9db1f90e99ede
TylerMurray1 <34780822+TylerMurray1@users.noreply.github.com>
2018-11-30 15:27:58 -0500

    Serial Communication, Blo;5Dckchain updates

--- server.js
+++ server.js
@@ -1,18 +1,25 @@
 //DO NOT MOVE THIS FROM THE ROOT DIRECTORY WITHOUT CONSULTING SAM
 const express = require('express');
+const Blockchain = require('./blockchain/blockchain');
 var bodyParser = require("body-parser");
 var jsrender = require('jsrender');
 const CONFIG = require('./public/config.json');
 const app = express();
 var userObj = require('./public/users.json');
+var adminObj = require('./public/admins.json');
 var fs = require('fs')
 var path = require('path');
-var SerialPort = require('serialport');
 
 //TODO: I'd like to make this a dictionary that associates IDs with user names - Sam
 
 var authenticated = false;
 var timeleft = 5;
+
+var dataStore = "";
+var rfidTag = "";
+var weight = "";
+
+const myChain = new Blockchain();
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
@@ -25,22 +32,42 @@ app.get('/', function(req, res){
 )
 app.get('/logout', (req, res) => res.sendFile(CONFIG.userFilePath+'/HAZMAT/public/logout.html'))
 
-app.get('/newUser', (req, res) => res.sendFile(CONFIG.userFilePath+'/HAZMAT/public/newUser.html'))
+app.get('/transaction', (req, res) => {
+  res.sendFile(CONFIG.userFilePath+'/HAZMAT/public/transaction.html')
+})
 
-app.get('/weigh', function (req, res) {  
+app.get('/removeItem', (req, res) => {
+  const SerialPort = require('serialport')
+  const Readline = require('@serialport/parser-readline')
+  const port = new SerialPort('/dev/cu.usbmodem14201')
 
-	var downloadTimer = setInterval(function() {
-		var tmpl = jsrender.templates('./public/weigh.html');
-		var html = tmpl.render({wait_time: (timeleft)});
-		timeleft = timeleft - 1;
-		res.redirect('http://localhost:3000/weigh');
-		
-		if(timeleft < 1) {
-			res.redirect('http://localhost:3000/sensor_calculation');
-			clearInterval(downloadTimer);
-		}	
-	},1000);
-	
+  const parser = port.pipe(new Readline({ delimiter: '\r\n' }))
+	parser.on('data', function() {
+    dataStore = parser.buffer.toString('utf8');
+    dataArray = dataStore.split(": ");
+    rfidTag = dataArray[0];
+    weight = dataArray[1];
+  });
+  res.sendFile(CONFIG.userFilePath+'/HAZMAT/public/removeItem.html')
+})
+
+app.get('/returnItem', (req, res) => {
+  const SerialPort = require('serialport')
+  const Readline = require('@serialport/parser-readline')
+  const port = new SerialPort('/dev/cu.usbmodem14201')
+
+  const parser = port.pipe(new Readline({ delimiter: '\r\n' }))
+	parser.on('data', function() {
+    dataStore = parser.buffer.toString('utf8');
+    dataArray = dataStore.split(": ");
+    rfidTag = dataArray[0];
+    weight = dataArray[1];
+  });
+  res.sendFile(CONFIG.userFilePath+'/HAZMAT/public/returnItem.html')
+})
+
+app.get('/admin', (req, res) => {
+  res.sendFile(CONFIG.userFilePath+'/HAZMAT/public/admin.html')
 })
 
 app.get('/sensor_calculation', function (req, res) {
@@ -48,30 +75,31 @@ app.get('/sensor_calculation', function (req, res) {
     res.redirect('http://localhost:3000/');
   }
   else{
-	
-	//var Readline = SerialPort.parsers.Readline;
-	//var parser = new Readline();
-	//var port = new SerialPort('/dev/ttyAMA0', {
-		//baudRate: 9600,
-		//parser: parser,
-	//});
-	//port.on('readable', function () {
-		//res.sendFile(CONFIG.userFilePath+'/HAZMAT/public/sensor_calculation.html')
-		//var tmpl = jsrender.templates('./public/sensor_calculation.html');
-		//var html = tmpl.render({weight: port.read().decode(), rfid_tag: "1234567"});
-		//res.send(html);
-	//});
-	var tmpl = jsrender.templates('./public/sensor_calculation.html');
-	var html = tmpl.render({weight: "15", rfid_tag: "1234567"});
-	res.send(html);
+    myChain.addBlock(dataStore);
+    var fileContent = JSON.stringify(myChain);
+    fs.appendFile("./blockchain/store.json", fileContent, (err) => {
+      if (err) {
+          console.error(err);
+          return;
+      };
+    });
+  	var tmpl = jsrender.templates('./public/sensor_calculation.html');
+  	var html = tmpl.render({weight: weight, rfid_tag: rfidTag});
+  	res.send(html);
   }
 })
 
 app.post('/', function (req, res) {
-  console.log(Object.values(userObj).indexOf(req.body.id_value))
-  if(Object.values(userObj).indexOf(req.body.id_value) > -1){
+  //console.log(Object.values(userObj).indexOf(req.body.id_value))
+  if((Object.values(userObj).indexOf(req.body.id_value) > -1 && Object.keys(userObj).indexOf(req.body.id_value) > -1 )
+  || (Object.values(adminObj).indexOf(req.body.id_value) > -1 && Object.keys(adminObj).indexOf(req.body.id_value) > -1)){
     authenticated = true;
-    res.redirect('http://localhost:3000/weigh');
+    if(Object.values(adminObj).indexOf(req.body.id_value) > -1){
+      res.redirect('http://localhost:3000/admin');
+    }
+    else{
+      res.redirect('http://localhost:3000/transaction');
+    }
   }
   else {
 	  //retry login
@@ -82,9 +110,32 @@ app.post('/', function (req, res) {
 
 app.post('/saveJSON', (req, res) => {
   res.json({ ok: true });
-  fs.writeFile('./public/users.json', JSON.stringify(req.body), function(err, data){
-    if (err) console.log(err);
-  });
+  //
+  fs.readFile('./public/users.json', function readFileCallback(err, data){
+    if (err){
+        console.log(err);
+    } else {
+    obj = JSON.parse(data);
+    obj[Object.keys(req.body)[0]] = Object.values(req.body)[0]
+    var json = JSON.stringify(obj);
+    fs.writeFile('./public/users.json', json);
+  }});
+});
+
+app.post('/deleteUserData', (req, res) => {
+  res.json({ ok: true });
+  //
+  fs.readFile('./public/users.json', function readFileCallback(err, data){
+    if (err){
+        console.log(err);
+    } else {
+    obj = JSON.parse(data);
+    console.log(obj)
+    delete obj[Object.keys(req.body)[0]]
+    console.log(obj)
+    var json = JSON.stringify(obj);
+    fs.writeFile('./public/users.json', json);
+  }});
 });
 
 app.listen(CONFIG.port, () => console.log(`Example app listening on port ${CONFIG.port}!`));
